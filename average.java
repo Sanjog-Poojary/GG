4b6bf2a9b041cacbdaf16fbc2cacebc581e64799
Sanjog-Poojary <2024.sanjog.poojary@ves.ac.in>
2025-07-23 10:17:49 +0530

    first draft of average

---
+++ average.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class average{
+        public static void main(String args[]){
+              int a,b,c;
+              Scanner sc = new Scanner(System.in);
+              a = sc.nextInt();
+              b = sc.nextInt();
+              c = (a+b)/2;
+              System.out.print("The average is: "+c);
+            }
+     }
+              
+              
